7b2f053f3bdd3c44d95cb033538dfd99790901ac
wanglingu520 <278517090@qq.com>
2018-05-06 22:20:08 +0800

    Force change style

--- do.go
+++ do.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-	var i int
-	var j int
-
-	for i = 2; i < 100; i++ {
-		for j = 2; j < i; j++ {
-			if i%j == 0 {
-				break
-			}
-		}
-		if j >= i {
-			fmt.Printf("%d\n", i)
-		}
+func Factorial(n uint64) (result uint64) {
+	if n > 0 {
+		result := n * Factorial(n-1)
+		return result
 	}
+	return 1
+}
+
+func main() {
+	var i uint64 = 15
+	//var i int =15
+
+	fmt.Printf("%d ! = %d\n", i, Factorial(i))
+	//Factorial(uint64(i))此处因为i的定义风格，与函数表达式相违背，在函数上需要进行强制转换
 }
