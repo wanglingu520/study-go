aab97469e0047db93ae1e42aa3ce82d944d0ca93
wanglingu520 <278517090@qq.com>
2018-05-06 15:31:48 +0800

    prime number

--- do.go
+++ do.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7}
-	PrintSlice(numbers)
-	fmt.Println("numbers ==", numbers)
-	number1 := numbers[1:4]
-	PrintSlice(number1)
-	fmt.Println("numberc[:3] ==", numbers[:3])
-	number2 := numbers[4:]
-	PrintSlice(number2)
-	number3 := make([]int, 0, 5)
-	PrintSlice(number3)
-	number4 := numbers[:2]
-	PrintSlice(number4)
+	var i int
+	var j int
+
+	for i = 2; i < 100; i++ {
+		for j = 2; j < i; j++ {
+			if i%j == 0 {
+				break
+			}
+		}
+		if j >= i {
+			fmt.Printf("%d\n", i)
+		}
+	}
 }
