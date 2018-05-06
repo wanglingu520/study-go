da1bf9c636718a900d19a62b390c2dad6f5e0cbd
wanglingu520 <278517090@qq.com>
2018-05-06 11:11:59 +0800

    new one

--- do.go
+++ do.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	a, b := swap("Mahesh", "Kumar")
-	fmt.Println(a, b)
-}
-func swap(x, y string) (string, string) {
-	return y, x
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7}
+	PrintSlice(numbers)
+	fmt.Println("numbers ==", numbers)
+	number1 := numbers[1:4]
+	PrintSlice(number1)
+	fmt.Println("numberc[:3] ==", numbers[:3])
+	number2 := numbers[4:]
+	PrintSlice(number2)
+	number3 := make([]int, 0, 5)
+	PrintSlice(number3)
+	number4 := numbers[:2]
+	PrintSlice(number4)
 }
