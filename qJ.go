bcf06cb116e390c6e5e796787baa27056a789982
wanglingu520 <278517090@qq.com>
2018-05-06 15:40:34 +0800

    multiplication table

--- qJ.go
+++ qJ.go
@@ -2,21 +2,15 @@ package main
 
 import "fmt"
 
-func test() {
-	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
-	PrintSlice(numbers)
-	fmt.Println("numbers==", numbers)
-	number1 := numbers[1:4]
-	PrintSlice(number1)
-	fmt.Println("number[:3] ==", numbers[:3])
-	number2 := numbers[4:]
-	PrintSlice(number2)
-	number3 := make([]int, 0, 5)
-	PrintSlice(number3)
-	number4 := numbers[:2]
-	PrintSlice(number4)
-}
+func main() {
+	var b int
+	var a int
 
-func PrintSlice(x []int) {
-	fmt.Printf("len = %d cap = %d slice = %v\n", len(x), cap(x), x)
+	for a = 1; a < 10; a++ {
+		for b = 1; b < 10; b++ {
+			fmt.Printf("%d * %d = %d\n", a, b, a*b)
+		}
+
+	}
+	return
 }
