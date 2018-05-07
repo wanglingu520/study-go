0012995fea9a6d0fff8d1009328db6da7e6341dd
wanglingu520 <278517090@qq.com>
2018-05-07 19:36:36 +0800

    Common Divisor

--- do.go
+++ do.go
@@ -1,45 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
-
-type DivideError struct {
-	dividee int
-	divider int
-}
-
-func (de *DivideError) Error() string {
-	strFormat := `
-    Cannot proceed, the divider is zero.
-    dividee: %d
-    divider: 0
-`
-	return fmt.Sprintf(strFormat, de.dividee)
-}
-
-func Divide(varDividee int, varDivider int) (result int, errorMsg string) {
-	if varDivider == 0 {
-		dData := DivideError{
-			dividee: varDividee,
-			divider: varDivider,
-		}
-		errorMsg = dData.Error()
-		return
-	} else {
-		return varDividee / varDivider, ""
-	}
-
-}
+import "fmt"
 
 func main() {
+	var a int = 84
+	var b int = 36
 
-	if result, errorMsg := Divide(100, 10); errorMsg == "" {
-		fmt.Println("100/10 = ", result)
+	fmt.Println(gcd(a, b))
+}
+
+func gcd(x, y int) int {
+	for y != 0 {
+		x, y = y, x%y
 	}
-
-	if _, errorMsg := Divide(100, 0); errorMsg != "" {
-		fmt.Println("errorMsg is: ", errorMsg)
-	}
-
+	return x
 }
