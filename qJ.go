7d6806549ac88c4f6d22e71ee474659c3800ca81
wanglingu520 <278517090@qq.com>
2018-05-08 16:53:06 +0800

    mustCopy

--- qJ.go
+++ qJ.go
@@ -4,32 +4,20 @@ import (
 	"io"
 	"log"
 	"net"
-	"time"
+	"os"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	conn, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-		handleConn(conn)
-	}
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
 }
 
-func handleConn(c net.Conn) {
-	defer c.Close()
-	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
-		if err != nil {
-			return
-		}
-		time.Sleep(1 * time.Second)
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
 	}
 }
