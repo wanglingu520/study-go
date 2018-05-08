73e520ff0bb0ff14cd88677ee7a7c06f865be205
wanglingu520 <278517090@qq.com>
2018-05-08 09:21:35 +0800

    Jason movie practice

--- do.go
+++ do.go
@@ -1,17 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
-func main() {
-	var a int = 84
-	var b int = 36
-
-	fmt.Println(gcd(a, b))
+type Movie struct {
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json:"color,omitempty"`
+	Actors []string
 }
 
-func gcd(x, y int) int {
-	for y != 0 {
-		x, y = y, x%y
+func main() {
+	var movies = []Movie{
+		{Title: "Casablanca", Year: 1942, Color: false,
+			Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+		{Title: "Cool Hand Luke", Year: 1967, Color: true,
+			Actors: []string{"Paul Newman"}},
 	}
-	return x
+
+	byteString, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	fmt.Printf("%s\n", string(byteString))
+
+	var film Movie
+	var str string = `{"released":1942,"color,omitempty":false}`
+	err = json.Unmarshal([]byte(str), &film)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	fmt.Printf("film released : %d, color,omitempty : %v\n", film.Year, film.Color)
+
 }
