a9a75d8d1bf5fab3e287b5b3001aabcbb5912aae
wanglingu520 <278517090@qq.com>
2018-05-09 16:00:11 +0800

    Add second change
    ----introduce"  string.Join(os.Args[]," ")  "

--- hello.go
+++ hello.go
@@ -3,16 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	var s string = "hi"
-	var sep string
-
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = ""
-		// sep为空字符串，如何让其显示添加字符,给s赋值，如添加hi
-	}
-	fmt.Println(s)
+	fmt.Println("hi", strings.Join(os.Args[1:], " "))
 }
