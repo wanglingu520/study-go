6cc16bc63de6a13596f8c32db4a02cc0ba4dd04a
wanglingu520 <278517090@qq.com>
2018-05-10 10:26:14 +0800

    Path

--- hello.go
+++ hello.go
@@ -9,17 +9,28 @@ type Point struct {
 	X, Y float64
 }
 
-func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
-}
-
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+type Path []Point
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
 func main() {
-	var a = Point{7, 6}
-	var b = Point{5, 4}
-	fmt.Printf("p to q distance = %f\n", Distance(a, b))
-	fmt.Println(a.Distance(b))
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+	fmt.Println(perim.Distance())
 }
