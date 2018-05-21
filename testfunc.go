1b28b033313e52db0abd97540f9ba3cbb7710358
wanglingu520 <278517090@qq.com>
2018-05-21 10:03:47 +0800

    welcome to http://192.168.1.2:8080

--- testfunc.go
+++ testfunc.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Printf("hello word\n")
-	fmt.Printf("goodbye word")
+	fmt.Println("Please	visit	http://192.168.1.2:8080/")
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		s := fmt.Sprintf("你好,	世界!	--	Time:	%s", time.Now().String())
+		fmt.Fprintf(w, "%v\n", s)
+		log.Printf("%v\n", s)
+	})
+	if err := http.ListenAndServe(":12345", nil); err != nil {
+		log.Fatal("ListenAndServe:	", err)
+	}
 }
